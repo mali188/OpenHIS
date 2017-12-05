cb1282b903b98ee685e9c32eaeb76008e122dcbe
malixiao <13717866946@163.com>
2017-12-05 10:17:58 +0800

    只兼容php版本>=5.5.0

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 // 应用入口文件
 
 // 检测PHP环境
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+if(version_compare(PHP_VERSION,'5.5.0','<'))  die('require PHP > 5.5.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',true);
@@ -21,7 +21,6 @@ define('APP_DEBUG',true);
 define('APP_PATH','./Application/');
 define('WEB_ROOT_PATH',__DIR__.'/');
 
-//define('BIND_MODULE','Admin');
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
